8c200795a15a024e52085846ee586427edbfa120
1BENJAMINFUENTES <124859816+1BENJAMINFUENTES@users.noreply.github.com>
2024-08-12 19:33:44 -0600

    x

---
+++ scripts/login.js
@@ -0,0 +1,9 @@
+function showForm() {
+    document.getElementById('loginForm').classList.add('show');
+    document.getElementById('loginForm').classList.remove('hide');
+}
+
+function hideForm() {
+    document.getElementById('loginForm').classList.add('hide');
+    document.getElementById('loginForm').classList.remove('show');
+}
